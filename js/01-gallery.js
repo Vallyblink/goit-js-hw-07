b45ee47c9b24d697fc7b27a028c9149480f5cd01
Vallyblink <alabama.toj@gmail.com>
2023-03-20 21:04:37 +0200

    first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Change code below this line  
+const galleryList = document.querySelector(".gallery")
+ 
+const pictureElement = galleryItems.map(({preview, original, description }) => 
+`<li class="gallery__item">
+   <a class ="gallery__link" href="${original}">
+   <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a> 
+</li>`).join("");
+let instance = '';
+galleryList.insertAdjacentHTML("afterbegin", pictureElement)
 
-console.log(galleryItems);
+
+galleryList.addEventListener("click",onModalOpne)
+
+function onModalOpne(event){
+ event.preventDefault();
+
+ if ( event.target !== event.currentTarget ){
+    window.addEventListener('keydown',onEscapeExit);
+   let imageLarge = event.target.getAttribute('data-source');
+    instance = basicLightbox.create(`
+    <img src=${imageLarge} width="800" height="600">
+    `);
+     instance.show(); }
+}
+
+function onEscapeExit(button){ 
+    if (button.code === 'Escape' || button.code === 'Space' ){
+        instance.close();
+        window.removeEventListener('keydown', onEscapeExit);
+    }
+};
+   
