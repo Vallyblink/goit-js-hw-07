65d565db9c66f8d5d5f2123390121b73cfff20ae
Vallyblink <alabama.toj@gmail.com>
2023-03-21 15:17:36 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector(".gallery")
 
-console.log(galleryItems);
+ 
+
+const pictureElement = galleryItems.map(({preview, original, description }) => 
+`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`).join("");
+
+galleryList.insertAdjacentHTML("afterbegin", pictureElement)
+
+const gallery = new SimpleLightbox(".gallery a", {
+	captions: true,
+	captionSelector: "img",
+	captionType: "attr",
+	captionsData: "alt",
+	captionPosition: "bottom",
+	captionDelay: 250,
+});
+
